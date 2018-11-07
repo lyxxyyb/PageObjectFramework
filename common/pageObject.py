9ff81a88c0864dfe15f18e50ceb0aac4c557ed51
lily <lilinyu861@outlook.com>
2018-11-07 15:48:32 +0800

    Design Patterns

---
+++ common/pageObject.py
@@ -0,0 +1,14 @@
+# coding = utf8
+import selenium
+
+
+class pageObject():
+    def __init__(self):
+        pass
+
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
---
+++ test/user_baidu.py
@@ -0,0 +1,13 @@
+# coding = utf8
+from common.pageObject import *
+
+
+class test():
+    def __init__(self):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
