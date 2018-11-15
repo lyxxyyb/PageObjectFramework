1e39c890ae5e6898f8087f114f4a71d830388060
lily <lilinyu861@outlook.com>
2018-11-15 20:07:30 +0800

    修改pageObject

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,6 +1,12 @@
 # coding = utf-8
-from common.pageObject import PageObject
+from common.pageObject import PageObject,PageElement
+from common.url import *
 
 
-class LoginPage():
-    pass
+class LoginPage(PageObject):
+    base_url = Url().base_url
+    url = base_url + '/login2'
+
+    # login页面元素
+    email = PageElement(id='inputEmail')
+    passwd = PageElement(id='inputPassword')
